638f18a5705fca4db33b2791c64bf3eadd3575af
leoliang <liangxiaopeng@gmail.com>
2017-01-08 18:34:26 +0800

    配置文件更新

--- config/config.js
+++ config/config.js
@@ -1,8 +1,17 @@
 module.exports = {
     'ssh2_route': {
         'host': '192.168.1.1',
-        'username': 'root',
+        'username': '',
         'privateKey': process.env.HOME + '/.ssh/id_rsa',
         'port': 22
+    },
+    'forecast': {
+        service: 'darksky',
+        key: '',
+        units: 'celcius',
+        lang: 'zh'
+    },
+    'flickr': {
+        'apiKey': ''
     }
 };
